cd784108086f0d7899840ee42211a1de61b37fcf
datllcom <35055114+datllcom@users.noreply.github.com>
2018-03-07 23:30:12 +0800

    Create yltalks.com.jQ.js
    英伦电影授权

---
+++ pp/datll-pp_pc/js/yltalks.com.jQ.js
@@ -0,0 +1,30 @@
+
+$win = document.domain;
+if($win.indexOf('yltalks.com')<0&&$win.indexOf('yltalks.com')<0){
+        alert("采集联盟提示:请购买正版模板！QQ：834023388");
+location.href='http://t.cn/REfNqUr';//不是设定的域名就会跳转
+}
+$.extend({ 
+     includePath: '//datllcom.github.io/template/', 
+         includeroute: 'pp/datll-pp_pc/js/', 
+     include: function(file) { 
+        var files = typeof file == "string" ? [file]:file; 
+        for (var i = 0; i < files.length; i++) { 
+            var name = files[i].replace(/^\s|\s$/g, ""); 
+            var att = name.split('.'); 
+            var ext = att[att.length - 1].toLowerCase(); 
+            var isCSS = ext == "css"; 
+            var tag = isCSS ? "link" : "script"; 
+            var attr = isCSS ? " type='text/css' rel='stylesheet' " : " language='javascript' type='text/javascript' "; 
+            var link = (isCSS ? "href" : "src") + "='" + $.includePath + $.includeroute + name + "'"; 
+            var s = "<" + tag + attr + link + "></" + tag + ">"; 
+            if ($(tag + "[" + link + "]").length == 0) document.write(s); 
+                        
+        } 
+   } 
+});
+$.include('jquery.min.js'); 
+$.include('pp/vendor.js'); 
+$.include('pp/header.js'); 
+$.include('pp/home.js'); 
+$.include('amazeui.min.js');
