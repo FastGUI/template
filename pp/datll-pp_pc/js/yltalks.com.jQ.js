91cfcb5b8f5fafbf8fd47b3bd7b321a92fe15640
datllcom <35055114+datllcom@users.noreply.github.com>
2018-03-07 23:31:51 +0800

    Update yltalks.com.jQ.js
    英伦

--- pp/datll-pp_pc/js/yltalks.com.jQ.js
+++ pp/datll-pp_pc/js/yltalks.com.jQ.js
@@ -5,7 +5,7 @@ if($win.indexOf('yltalks.com')<0&&$win.indexOf('yltalks.com')<0){
 location.href='http://t.cn/REfNqUr';//不是设定的域名就会跳转
 }
 $.extend({ 
-     includePath: '//datllcom.github.io/template/', 
+     includePath: '//raw.githubusercontent.com/datllcom/template/master/', 
          includeroute: 'pp/datll-pp_pc/js/', 
      include: function(file) { 
         var files = typeof file == "string" ? [file]:file; 
